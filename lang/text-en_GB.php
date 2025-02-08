397b4a54a0faff4ce5716314cffbc3d70565fc68
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-08 15:57:44 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'TinyMCE',
+    '{description}' => 'WYSIWYG editor',
+
+    // Settings
+    '{settings.title}' => 'Widget settings "TinyMCE"',
+    // Settings: поля
+    'Default toolbar' => 'Default toolbar',
+    'Show toolbar' => 'Show toolbar',
+    'Show menubar' => 'Show menubar',
+    'Show statusbar' => 'Show statusbar',
+    'Add plugins' => 'Add plugins (plugins)',
+    'Toolbar' => 'Toolbar',
+    'Toolbar (1 row)' => 'Toolbar (1 row)',
+    'Toolbar (2 row)' => 'Toolbar (2 row)',
+    'Editor TinyMCE' => 'Editor TinyMCE',
+    'Toolbar size' => 'Toolbar size',
+    'Extended valid elements' => 'Extended valid elements (extended_valid_elements)',
+    'Zoom image on click' => 'Zoom image on click',
+    'Zoom flag attribute' => 'Zoom flag attribute',
+    'An attribute will be added to the image tag. Telling the plugin that the image can be enlarged.' 
+        => 'An attribute will be added to the image tag. Telling the plugin that the image can be enlarged.',
+    'Full image address attribute' => 'Full image address attribute',
+    'An attribute will be added to the image tag that points the plugin to the URL of the full image.' 
+        => 'An attribute will be added to the image tag that points the plugin to the URL of the full image.',
+    'Template for getting full image file' => 'Template for getting full image file',
+    'All words separated by "," will be removed from the original image URL, resulting in a new URL for the full image.' 
+        => 'All words separated by "," will be removed from the original image URL, resulting in a new URL for the full image.',
+    'Parameters passed to the plugin responsible for enlarging the image' => 'Parameters passed to the plugin responsible for enlarging the image',
+    'Small' => 'Small size',
+    'Medium' => 'Medium size',
+    'Large' => 'Large size',
+    'site' => 'site',
+    'version' => 'version',
+    // Settings: всплывающие сообщения / текст
+    'Widget settings successfully changed' => 'Widget settings "TinyMCE" successfully changed.'
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения GearMagic.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'TinyMCE',
+    '{description}' => 'Редактор WYSIWYG',
+
+    // Settings
+    '{settings.title}' => 'Настройка виджета "TinyMCE"',
+    // Settings: поля
+    'Default toolbar' => 'Инструменты по умолчанию',
+    'Show toolbar' => 'Показывать панель инструментов',
+    'Show menubar' => 'Показывать главное меню',
+    'Show statusbar' => 'Показывать строку статуса',
+    'Add plugins' => 'Подключаемые плагины (plugins)',
+    'Toolbar' => 'Панель инструментов',
+    'Toolbar (1 row)' => 'Панель инструментов (1-я строка)',
+    'Toolbar (2 row)' => 'Панель инструментов (2-я строка)',
+    'Editor TinyMCE' => 'Редактор TinyMCE',
+    'Toolbar size' => 'Размер инструментов',
+    'Extended valid elements' => 'Расширенная проверка элементов (extended_valid_elements)',
+    'Zoom image on click' => 'Увеличение изображения при нажатии',
+    'Zoom flag attribute' => 'Атрибут признака увеличения',
+    'An attribute will be added to the image tag. Telling the plugin that the image can be enlarged.' 
+        => 'В тег изображения будет добавлен атрибут. Указывающий плагину на возможность увеличения изображения.',
+    'Full image address attribute' => 'Атрибут адреса полного изображения',
+    'An attribute will be added to the image tag that points the plugin to the URL of the full image.' 
+        => 'В тег изображения будет добавлен атрибут. Указывающий плагину URL-адрес полного изображения.',
+    'Template for getting full image file' => 'Шаблон получения файла полного изображения',
+    'All words separated by "," will be removed from the original image URL, resulting in a new URL for the full image.' 
+        => 'Из исходного URL-адреса изображения будут удалены все слова, указанные через разделитель ",". Таким образом будет получен новый URL-адрес полного изображения.',
+    'Parameters passed to the plugin responsible for enlarging the image' => 'Параметры передаваемые плагину, отвечающий за увилечение изображения',
+    'Small' => 'Малый размер',
+    'Medium' => 'Средний размер',
+    'Large' => 'Большой размер',
+    'site' => 'сайт',
+    'version' => 'версия',
+    
+    // Settings: всплывающие сообщения / текст
+    'Widget settings successfully changed' => 'Настройки виджета "TinyMCE" успешно сохранены.'
+];
