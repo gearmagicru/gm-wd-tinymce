38f88ecb20bf74397b0f06cd5f005d64379aa37d
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-08 15:57:03 +0300

    Файл конфигурации версии виджета

---
+++ config/.version.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения GearMagic.
+ * 
+ * Файл конфигурации версии виджета.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    'name'        => 'TinyMCE',
+    'description' => 'WYSIWYG editor',
+    'version'     => '1.0',
+    'versionDate' => '20-12-2017',
+    'author'      => 'GearMagic',
+    'authorUrl'   => 'https://gearmagic.ru',
+    'email'       => 'anton.tivonenko@gmail.com',
+    'url'         => 'https://apps.gearmagic.ru/component/gm-wd-tinymce',
+    'license'     => 'The MIT License',
+    'licenseUrl'  => 'https://apps.gearmagic.ru/license/mit-license.html'
+];
