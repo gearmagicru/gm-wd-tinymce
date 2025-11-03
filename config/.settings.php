e1f04b472433c4e79cd95542f5509fa2c23f396d
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-11-03 19:35:29 +0300

    Изменён config/.settings,php

--- config/.settings.php
+++ config/.settings.php
@@ -6,7 +6,7 @@
  * 
  * @link https://gearmagic.ru
  * @copyright Copyright (c) 2015 Веб-студия GearMagic
- * @license https://gearmagic.ru/license/
+ * @license https://gearmagic.ru/license.html
  */
 
 return [
